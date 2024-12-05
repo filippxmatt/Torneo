e949b5c6b30b1142238c497b5af59ed78ef82dbf
Filippo Mattioli <filippomattioli06@gmail.com>
2024-12-05 09:33:53 +0100

    scrittura classi Leggi e Scrivi

--- src/torneo/LeggiFile.java
+++ src/torneo/LeggiFile.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package torneo;
+package gestionefile;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
- * @author B045_10
+ * @author MC
+ * @ 12/01/23
  */
-public class LeggiFile {
+
+public class LeggiFile extends Thread{
+    String nomeFile;
     
+    public LeggiFile(String nomeFile){
+        this.nomeFile = nomeFile;
+    }
+    
+    /**
+     * Legge il file senza tener conto del tipo di file
+     * e lo mostra in output
+     */
+    public void leggi(){
+        FileReader fr;
+        int i; 
+        try { 
+            //1) apro il file
+            fr = new FileReader(nomeFile);
+            //2) leggo carattere per carattere e lo stampo 
+            while ((i=fr.read()) != -1)
+                System.out.print((char) i);
+            
+            System.out.print("\n\r");
+            //3) chiudo il file
+            fr.close();
+        } catch (IOException ex) {
+            System.err.println("Errore in lettura!");
+        }
+    }
+    
+
+    public void run(){
+        leggi();
+    }
 }
